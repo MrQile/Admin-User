3386444b5dded2d0ba7d863afd60bf1bca9e0f30
MrQile <quitalizner@gmail.com>
2017-09-02 11:38:39 -0700

    Add login logout

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,21 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+  	user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+    	log_in user
+    	redirect_to root
+    else
+	    flash.now[:danger] = 'Invalid email/password combination'    #use flash.now for rendering views
+	    render 'new'			#render is not considered as a request and since flash messages persist for one request, they will be visible for render and not for redirect
+  end
+
+  def destroy
+  	log_out
+  	redirect_to root
+  end
+
+  
+end
